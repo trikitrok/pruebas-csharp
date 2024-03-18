95376597cda7924bfe448e33a3bfea7ca7cdfb71
trikitrok <trikitrok@gmail.com>
2024-03-18 19:24:21 +0100

    100% branch coverage

--- TestExamples/CounterTest.cs
+++ TestExamples/CounterTest.cs
@@ -7,7 +7,7 @@ public class CounterTest
     [Test]
     [TestCase(new int[] {}, 0)] // empty
     [TestCase(null, 0)]
-    //[TestCase(new int[] {1, 2, 2, 2}, 1)] // one clump
+    [TestCase(new int[] {1, 2, 2, 2}, 1)] // one clump
     [TestCase(new int[] {1}, 0)] // one element
     public void counting_clumps(int[] nums, int result)
     {
